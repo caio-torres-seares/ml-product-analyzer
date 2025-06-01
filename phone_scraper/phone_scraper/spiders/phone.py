2c99beb493d573a8628b1204f34e7c931c6a9ac0
Caio Torres Seares <caiot.seares@gmail.com>
2025-06-01 14:21:31 -0300

    feat: add initial phone spider with basic data extraction (brand, name, old_price, new_price, link)

--- phone_scraper/phone_scraper/spiders/phone.py
+++ phone_scraper/phone_scraper/spiders/phone.py
@@ -7,4 +7,21 @@ class PhoneSpider(scrapy.Spider):
     start_urls = ["https://lista.mercadolivre.com.br/celular?sb=all_mercadolibre#D[A:celular]"]
 
     def parse(self, response):
-        pass
+
+        products = response.css('div.ui-search-result__wrapper')
+
+        for product in products:
+
+            brand = product.css('span.poly-component__brand::text').get()
+            name = product.css('a.poly-component__title::text').get()
+            old_price = product.css('div.poly-component__price s span.andes-money-amount__fraction::text').get()
+            new_price = product.css('div.poly-price__current span.andes-money-amount__fraction::text').get()
+            link = product.css('a.poly-component__title::attr(href)').get()
+            yield {
+                'brand' : brand,
+                'name': name,
+                'old_price': old_price,
+                'new_price': new_price,
+                'link': link
+            }
+
